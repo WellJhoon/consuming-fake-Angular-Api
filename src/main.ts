dd74f320d1521ccc57f3721a126cd5e0428e5afd
jhon <jhon-43769@outlook.com>
2023-02-16 17:14:26 -0400

    Prueba de coneeccion SSH De Github

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+/*Esto Es para una prueba */
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
